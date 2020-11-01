d125d04a959e2d5e3fdca7f55902f29fb9054274
iru <irucondori.12>
2020-11-01 17:24:27 -0300

    avance

---
+++ src/main/java/ar/edu/unju/edm/dao/imp/ClienteDaoImp.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.edm.dao.imp;
+
+import ar.edu.unju.edm.dao.IClienteDao;
+
+public class ClienteDaoImp implements IClienteDao{
+
+}
---
+++ src/main/java/ar/edu/unju/edm/dao/imp/DetalleFacturaDaoImp.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.edm.dao.imp;
+
+import ar.edu.unju.edm.dao.IDetalleFacturaDao;
+
+public class DetalleFacturaDaoImp implements IDetalleFacturaDao {
+	
+}
